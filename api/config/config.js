997ebe143981c0e260df20cfb6552a20aad87694
xavierNabais <xaviernabais0@gmail.com>
2024-05-28 22:55:18 +0100

    remover pasta api

--- api/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: "N#h4K$2P@rJ7&fL8G%qW3cD9mE5tY!u",
-    timer: 600000, //10 minutos
-  };
--- api/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    db: '3700barber'
-}
